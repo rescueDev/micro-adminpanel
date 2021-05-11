a4f1a491fe663f474bad5d1e1854ef34227aa206
sa-borgia3 <sa.borgia3@gmail.com>
2021-05-11 15:20:33 +0200

    factory and seeder db

---
+++ database/seeds/EmployeeSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Company;
+use App\Employee;
+use Illuminate\Database\Seeder;
+
+class EmployeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Employee::class, 10)
+        ->make()
+        ->each(function($employee) {
+            $company = Company::inRandomOrder()->first();
+            $employee->company()->associate($company);
+            $employee->save();
+        });
+    }
+}
