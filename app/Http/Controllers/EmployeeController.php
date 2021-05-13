b73ec7de479a877b7a0d2b936bdcece8b290aaa1
sa-borgia3 <sa.borgia3@gmail.com>
2021-05-13 10:18:56 +0200

    ordering list desc

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -16,7 +16,7 @@ class EmployeeController extends Controller
      */
     public function index()
     {
-        $employees = Employee::paginate(10);
+        $employees = Employee::orderBy('id', 'desc')->paginate(10);
         return view('employees.employees-index', compact('employees'));
     }
 
